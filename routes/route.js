f7f7b2eb8f3c149c1f20c335d7da89ddf91868ef
AnirudhSharmagithub01 <anirudhsharma857000@gmail.com>
2024-06-17 11:00:16 +0530

    login router

--- routes/route.js
+++ routes/route.js
@@ -6,12 +6,13 @@ const route = express.Router();
 // routes
 const { sendOtp } = require("../controllers/AuthControllers/sendotp");
 const { signup } = require("../controllers/AuthControllers/sigup");
-
+const {login} = require("../controllers/AuthControllers/login");''
 
 
 
 route.post('/signup', signup);
 route.post('/sendOtp', sendOtp);
+route.post('/login',login);
 
 
 module.exports = route;
